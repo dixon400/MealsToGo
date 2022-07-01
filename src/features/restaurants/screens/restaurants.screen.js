93b22efa0f6dbc8554f2b9cc3268a76b90514434
Muyideen Khalifa <dixonmk400@gmail.com>
2022-07-01 06:54:29 +0100

    Add restaurant screen

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <RestaurantInfo />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    height: 300,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+});
